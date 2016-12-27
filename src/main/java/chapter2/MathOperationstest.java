3f22220330121421ed3b4ba60f7cbaadaafb8589
dmayanna <smdeekshith@gmail.com>
2016-12-27 15:23:11 -0700

    ds

--- src/main/java/chapter2/MathOperationstest.java
+++ src/main/java/chapter2/MathOperationstest.java
@@ -5,7 +5,7 @@ public class MathOperationstest {
 	public static void main(String[] args)
 	{
 		Addition add = new Addition();
-		System.out.println("hi "+add.add(2, 3));
+		System.out.println("hi this is Deekshith "+add.add(2, 3));
 		
 		Subtraction sub = new Subtraction();
 		System.out.println(sub.sub(5, 3));
